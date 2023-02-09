514ca402dfc7152bc19a4040989fe7a109a3f6eb
[Thomas <sonnleitner.thomas@gmx.at>
2023-02-09 13:03:37 +0100

    intermediate.final

--- Thomas/Intermediate/java.js
+++ Thomas/Intermediate/java.js
@@ -1,2 +1,15 @@
 const mydata = JSON.parse(employees);
 console.log(mydata)
+for (let i = 0; i < mydata.length; i++) {
+    document.getElementById('tbody').innerHTML +=
+        `<tr>
+        <th scope="row">${i+1}</th>
+        <td>${mydata[i].UniqueID}</td>
+        <td>${mydata[i].FirstName}</td>
+        <td>${mydata[i].LastName}</td>
+        <td>${mydata[i].email}</td>
+        <td>${mydata[i].jobTitle}</td>
+        <td>${mydata[i].Salary}</td>
+       
+    </tr>`
+}
