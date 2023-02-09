3e14507b400109e76b9a2ede7762d2c7774d3042
[Thomas <sonnleitner.thomas@gmx.at>
2023-02-09 14:35:39 +0100

    advanced

--- Thomas/Advanced/adv.js
+++ Thomas/Advanced/adv.js
@@ -1,5 +1,9 @@
 const mydata = JSON.parse(books)
 console.log(mydata)
 for (let i = 0; i < mydata.length; i++) {
-    document.getElementById('result').innerHTML += ` <br> <b>Title</b>: ${mydata[i].title} <br> <b>Author</b>: ${mydata[i].author} <br> <b>Status</b>: ${mydata[i].read} <br> <hr>`;
+    let color = "yellow";
+    if (mydata[i].read == "true") {
+        color = "green";
+    }
+    document.getElementById('result').innerHTML += ` <br> <b>Title</b>: ${mydata[i].title} <br> <b>Author</b>: ${mydata[i].author} <br> <b>Status</b>: ${mydata[i].read} <br> <div style="background: ${color}"> ${mydata[i].read} </div> <img src="${mydata[i].pic}" <br> <hr>`;
 }
